eb5d71b2def2eb01f380544fb3213754b161de5f
sibojia <jiasibo@sensetime.com>
2016-08-19 16:55:44 +0800

    `hanami generate app another`

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/static_path_test'
+require_relative '../apps/another/application'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
+  mount Another::Application, at: '/another'
   mount Web::Application, at: '/'
 end
